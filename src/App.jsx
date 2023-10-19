e367c3f6b8240845975c24147e73844660af84a3
zharris915 <zharrris915@yahoo.com>
2023-10-19 13:04:21 -0600

    footer and navbar attempt

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route, Link } from "react-router-dom"
 import Blue from "./Components/blue"
 import Red from "./Components/red"
 import Home from "./Components/Home"
+import Navbar from "./Components/Navbar"
+import Footer from "./Components/Footer"
 import './App.css'
 
 function App() {
@@ -10,18 +12,20 @@ function App() {
 
   return (
     <>
-    <div id="container">
+        <div id="container">
       <h1></h1>
       <div id="navbar">
         <Link to="/blue">Blue</Link>
         <Link to="/red">Red</Link>
         <Link to="/">Home</Link>
+        <Link to="/Navbar">Navbar</Link>
       </div>
       <div id="main-section">
         <Routes>
           <Route path="/blue" element={<Blue/>}/>
           <Route path="/red" element={<Red/>}/>
           <Route path="/" element={<Home/>}/>
+          <Route path="/Navbar" element={<Navbar/>}/>
         </Routes>  
       </div>
     </div>
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div className="nav-bar">
+        <h1>Navigation</h1> 
+        </div>
+    );
+}

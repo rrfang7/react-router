0ca83da88e9e915cb68d330b7c2b106ee03ff95c
Ralph Fang <ralphrainierfang@yahoo.com>
2023-10-19 12:54:01 -0400

    Set up Router Config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+// import { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
 import './App.css'
 
 function App() {
@@ -8,9 +8,14 @@ function App() {
   return (
     <>
     <div id="container">
-      <h1>Hello React Router!</h1>
+      <h1></h1>
       <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<h1>Blue</h1>}/>
+          <Route path="/red" element={<h1>Red</h1>}/>
+        </Routes>  
+      </div>
     </div>
     </>
   )
